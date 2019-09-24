b1b29ff01eddad25cb38e91797c638dce72d1620
Nicolas Hernandez <nicolas.carlos.hernandez@gmail.com>
2019-09-24 23:14:04 +0000

    Taller knn. distance_to_row

--- src/knn.cpp
+++ src/knn.cpp
@@ -8,10 +8,13 @@ using namespace std;
 
 KNNClassifier::KNNClassifier(unsigned int n_neighbors)
 {
+    this->neighbors = n_neighbors;
 }
 
 void KNNClassifier::fit(SparseMatrix X, Matrix y)
 {
+    this->training_samples = X;
+    this->training_labels = y;
 }
 
 
@@ -26,4 +29,18 @@ Vector KNNClassifier::predict(SparseMatrix X)
     }
 
     return ret;
+}
+
+
+
+Vector KNNClassifier::distance_to_row(Vector row)
+{
+    auto distances = Vector(this->training_samples.rows());
+    int cols = this->training_samples.cols();
+    SparseMatrix diffs;
+    for(int i = 0; i < cols; ++i) {
+          diffs.col(i) = this->training_samples.col(i) - row;
+          distances(i) = diffs.col(i).norm();
+    }
+    return distances;
 }
--- src/knn.h
+++ src/knn.h
@@ -11,4 +11,8 @@ public:
 
     Vector predict(SparseMatrix X);
 private:
+    Vector distance_to_row(Vector row);
+    int neighbors;
+    SparseMatrix training_samples;
+    Matrix training_labels;
 };
